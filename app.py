08ded9843af24218adf5580cb65b1cacbcf15c5d
Gunji6 <94582311+Gunji6@users.noreply.github.com>
2024-07-02 15:36:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,3 +21,34 @@ def recommend(movie):
             # fetch poster from API
             recommend_movies_poster.append(fetch_poster(movie_id))
         return recommend_movies , recommend_movies_poster
+
+movies_dict = pickle.load(open('movies.pkl','rb'))
+movies = pd.DataFrame(movies_dict)
+similarity = pickle.load(open('similarity.pkl','rb'))
+
+st.title('Movie Recommendation System')
+
+selected_movie_name  = st.selectbox(
+'How would you like to be connected',
+(movies['title'].values)
+)
+
+if st.button("Recommend"):
+    names,posters = recommend(selected_movie_name)
+
+    col1, col2, col3,col4 ,col5= st.columns(5)
+    with col1:
+        st.text(names[0])
+        st.image(posters[0])
+    with col2:
+        st.text(names[1])
+        st.image(posters[1])
+    with col3:
+        st.text(names[2])
+        st.image(posters[2])
+    with col4:
+        st.text(names[3])
+        st.image(posters[3])
+    with col5:
+        st.text(names[4])
+        st.image(posters[4])
